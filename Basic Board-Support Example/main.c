85dce9e1782543160b3116bd3b3cd9697ed895cf
blavin3 <bryanlavin8@gmail.com>
2020-12-02 22:45:55 +0000

    button inputs added

--- Basic Board-Support Example/main.c
+++ Basic Board-Support Example/main.c
@@ -1,33 +1,46 @@
 /* 
- * Basic code to control the digital outputs of TM4C123G
- *
- * Board support files allow the implementation of background
- * and forground software design. Seperate compliation speeds 
- * up compile time. This also will allow application code be 
- * ported to a new device easilyas the bsp would have to be 
- * updated to interface with the new device and the apllication
- * code does not need to be changed.
- *  
- * delay(unsigned int timeout) is a simple blocking delay fucntion
- * in future code implemnt one of the timer peripherals to free up
- * CPU i.e. check if an input has changed rather than wait for delay
- * to finish.
- *
+ * main.c
  */
 
 #include "bsp.h"
 
 int main()
 {
+  /* Configure GPIO */
   pin_setup(GPIODATA_F, RED_LED, OUTPUT);
   pin_setup(GPIODATA_F, BLUE_LED, OUTPUT);
+  pin_setup(GPIODATA_F, GREEN_LED, OUTPUT);
+  pin_setup(GPIODATA_F, WHITE_LED, OUTPUT);
+  pin_setup(GPIODATA_F, SW1, INPUT);
+  pin_setup(GPIODATA_F, SW2, INPUT);
   while(1)
   {
+    /* Only Light Red LED */
     pin_on(GPIODATA_F, RED_LED);
     pin_off(GPIODATA_F, BLUE_LED);
     delay(1000000);
+    
+    /* Only Light Blue LED */
     pin_off(GPIODATA_F, RED_LED);
     pin_on(GPIODATA_F, BLUE_LED);
     delay(1000000);
+    pin_off(GPIODATA_F, RED_LED);
+    pin_off(GPIODATA_F, BLUE_LED);
+    
+    /* If Button 1 Pressed Only Light Green LED */
+    if(pin_read(GPIODATA_F, SW1) == 1)
+    {
+      pin_on(GPIODATA_F, GREEN_LED);
+      delay(3000000);
+      pin_off(GPIODATA_F, GREEN_LED);
+    }
+    
+    /* If Button 2 Pressed Only Light White LED */
+    if(pin_read(GPIODATA_F, SW2) == 1)
+    {
+      pin_on(GPIODATA_F, WHITE_LED);
+      delay(3000000);
+      pin_off(GPIODATA_F, WHITE_LED);
+    }
   }
 }
